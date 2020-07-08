0276f10d7c3aadfc7acf507a388eff66599875e0
ZirvedaAytimur <zirvedaytimur@gmail.com>
2020-07-08 16:58:29 +0300

    Day 14 added.

---
+++ Day14/src/Solution.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Difference {
+    private int[] elements;
+    public int maximumDifference;
+
+    // Add your code here
+    Difference(int[] array){
+        elements = array;
+    }
+    public void computeDifference(){
+        Arrays.sort(elements);
+        maximumDifference = elements[elements.length - 1] - elements[0];
+    }
+
+} // End of Difference class
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        sc.close();
+
+        Difference difference = new Difference(a);
+
+        difference.computeDifference();
+
+        System.out.print(difference.maximumDifference);
+    }
+}
