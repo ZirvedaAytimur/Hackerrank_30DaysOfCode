f1797d6830cff0e0e7f44ab29e34f456e4738f71
ZirvedaAytimur <zirvedaytimur@gmail.com>
2020-08-10 13:59:22 +0300

    Pyhton added for Day7.

---
+++ Day7/main.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    arr.reverse()
+    for i in range(len(arr)):
+        print(arr[i], end=" ")
